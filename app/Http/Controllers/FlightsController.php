8399e180494b8b558c4c19a84f1b2f440dd252c5
FIRST_NAME LAST_NAME <jekyns@yandex.ru>
2021-06-13 22:14:23 +0300

    code refactoring

--- app/Http/Controllers/FlightsController.php
+++ app/Http/Controllers/FlightsController.php
@@ -7,6 +7,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Storage;
 use App\Models\Flight;
+use Illuminate\Support\Facades\Validator;
 
 class FlightsController extends Controller
 {
@@ -44,14 +45,25 @@ class FlightsController extends Controller
 
     public function upload(Request $request)
     {
-        $flightCsv = $request->file('flight_csv');
-        $fileName = explode('.', $flightCsv->getClientOriginalName());
-        $extension = array_pop($fileName);
-        if($extension !== 'csv'){
-            return json_encode(['error' => true, 'message' => 'Wrong file extension']);
+        $validator = Validator::make($request->file(), [
+            'flight_csv' => 'required|mimes:csv,txt',
+        ]);
+
+        if ($validator->fails()) {
+            return json_encode([
+                'error' => true,
+                'message' => 'flight_csv does not exist or is not a file',
+            ]);
         }
+
+        $flightCsv = $request->file('flight_csv');
+
         Storage::putFileAs('flights', new File($flightCsv), 'flight.csv');
-        $uploads = Artisan::call('parse:flights');
-        return json_encode(['error' => false, 'message' => $uploads ? 'Flights loaded' : 'There is nothing to add or update']);
+        $uploads = Flight::uploadCsv();
+        return json_encode([
+            'error' => false,
+            'message' => $uploads['result'] ? 'Flights loaded' : 'There is nothing to add or update',
+            'wrongRows' => $uploads['wrongRows']
+        ]);
     }
 }
--- app/Models/Flight.php
+++ app/Models/Flight.php
@@ -3,6 +3,10 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Facades\Validator;
 
 class Flight extends Model
 {
@@ -16,4 +20,50 @@ class Flight extends Model
         'ArrivalDate',
         'Number'
     ];
+
+    public static function uploadCsv () {
+        $flightCsv = explode(PHP_EOL, Storage::get('flights/flight.csv'));
+        array_shift($flightCsv);
+
+        $flights = [];
+        $wrongRows = [];
+        foreach ($flightCsv as $i => $row) {
+            if (!$row) {
+                continue;
+            }
+            $flight = explode(',', $row);
+
+            $validator = Validator::make($flight, [
+                '0' => 'required|integer',
+                '1' => 'required|string',
+                '2' => 'required|string',
+                '3' => 'required|date',
+                '4' => 'required|date_format:Hi',
+                '5' => 'required|date',
+                '6' => 'required|date_format:Hi',
+                '7' => 'required|string',
+            ]);
+            if ($validator->fails()) {
+                $wrongRows[] = $i;
+                Log::info('ERROR at ' . $i . ' row with flight id: ' . $flight[0]);
+                Log::info($validator->errors());
+                continue;
+            }
+
+            $DepartureTime = $flight[4] . '00';
+            $ArrivalTime = $flight[6] . '00';
+
+            $flights[] = [
+                'id' => $flight[0],
+                'Origin' => $flight[1],
+                'Destination' => $flight[2],
+                'DepartureDate' => $flight[3] . $DepartureTime,
+                'ArrivalDate' => $flight[5] . $ArrivalTime,
+                'Number' => $flight[7],
+            ];
+        }
+        $res = DB::table('flights')->upsert($flights, 'id');
+
+        return ['result' => !!$res, 'wrongRows' => $wrongRows];
+    }
 }
